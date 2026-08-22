6753a74cf517c9ea127a26da96b2f5f4b6669131
sk-hasmat-ali <firexhasmat@gmail.com>
2026-08-22 15:31:25 +0530

    meters done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let convertBtn=document.getElementById("convert-btn")
 let inputEle=document.getElementById("input-ele")
+let lengthEle=document.getElementById("length-ele")
 convertBtn.addEventListener("click",function(){
-    let num=Number(inputEle.value);
-    console.log(num)
+    let value=Number(inputEle.value)
+     let  feet=(value*3.28).toFixed(3)
+    let meter=(value/3.28).toFixed(3)
+     lengthEle.textContent=`${value}meter = ${feet}feet || ${value}feet = ${meter}meters  `
 
 })
